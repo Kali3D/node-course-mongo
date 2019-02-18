2c0516dbbfffb6c44d6c267c85f9523672c856a5
Nicolas Charpin <xleto@hotmail.com>
2019-02-18 12:43:56 +0100

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,8 +1,16 @@
 const {SHA256} = require("crypto-js");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
+const password = "Atreides_23";
 
+/*bcrypt.genSalt(10, (error, salt) => {
+	bcrypt.hash(password, salt, (error, hash) => console.log(hash));
+});
+*/
+const hashed = "$2a$10$cR7PVVFmJRyddXgVZcIAHeqVo.oFFeqtVVJFhXta3Ue.ki/Vrp29m";
 
+bcrypt.compare(password, hashed, (error, result) => console.log(result));
 
 // let message = "I'm user Nico";
 // const hash = SHA256(message).toString();
@@ -10,11 +18,11 @@ const jwt = require("jsonwebtoken");
 // console.log(message);
 // console.log(hash);
 
-const serverToken = "secretServerToken";
+// const serverToken = "secretServerToken";
 
-const data = {
-	id: 10
-};
+// const data = {
+// 	id: 10
+// };
 
 // const token = {
 // 	data,
@@ -33,7 +41,7 @@ const data = {
 // 	console.log("Data changed, ACHTUNG");
 
 
-const token = jwt.sign(data, serverToken);
-console.log(token);
-const decoded = jwt.verify(token, serverToken);
-console.log(decoded);
+// const token = jwt.sign(data, serverToken);
+// console.log(token);
+// const decoded = jwt.verify(token, serverToken);
+// console.log(decoded);
